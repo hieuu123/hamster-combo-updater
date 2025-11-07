de17bdf4b4542b06d2f544567581050a9fe4300b
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-11-07 14:50:06 +0700

    Update hamster_combo_cards.py

--- hamster_combo_cards.py
+++ hamster_combo_cards.py
@@ -8,8 +8,8 @@ import cloudscraper
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")   # username thật
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")   # app password thật
-POST_ID = 301012  # ID bài Hamster Kombat Combo Cards
-CHECK_TITLES = ["Studio podcast", "Develop your own engine", "CREATIVE CHAOS DAY"]  # Titles cũ để so sánh
+POST_ID = 301176  # ID bài Hamster Kombat Combo Cards
+CHECK_TITLES = ["Gaming chairs", "Invite mentors", "GAME ON"]  # Titles cũ để so sánh
 
 # ================= SCRAPE =================
 def scrape_combo():
